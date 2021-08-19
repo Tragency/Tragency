9caf990bb9a131761b43fc856a1e9dbda8a4c9a5
Vihaan Verma <vverma270705@gmail.com>
2021-08-19 10:35:29 +0530

    fixed bug

--- js/scripts.js
+++ js/scripts.js
@@ -148,59 +148,8 @@
   }
   /* -------------------
     Google map
-    ---------------------*/
-  $("#map").gmap3({
-    marker: {
-      address: "44 W 66th St, New York, NY",
-      options: { icon: "../img/assets/marker.png" },
-    },
-    map: {
-      options: {
-        styles: [
-          {
-            stylers: [{ saturation: -90 }, { lightness: 0 }, { gamma: 0.0 }],
-          },
-        ],
-        zoom: 13,
-        scrollwheel: false,
-        draggable: true,
-      },
-    },
-  });
   /* -------------------
     Animated progress bars
-    ---------------------*/
-  $(".progress-bars").waypoint(
-    function () {
-      $(".progress").each(function () {
-        $(this)
-          .find(".progress-bar")
-          .animate(
-            {
-              width: $(this).attr("data-percent"),
-            },
-            800
-          );
-      });
-    },
-    { offset: "100%", triggerOnce: true }
-  );
-  /* -------------------
-    Fun facts counter
-    ---------------------*/
-  $(".counter").counterUp({
-    delay: 8,
-    time: 1400,
-  });
-  /* -------------------
-    Video section lightbox
-    ---------------------*/
-  $("#video-lightbox").cubeportfolio({
-    gridAdjustment: "alignCenter",
-    lightboxDelegate: ".cbp-lightbox",
-    lightboxGallery: true,
-    lightboxShowCounter: false,
-  });
   /* -------------------
     Scroll functions
     ---------------------*/
@@ -340,35 +289,35 @@
   /* -------------------
     Subscribe form
     ---------------------*/
-  $(document).on("ready", function () {
-    $("#subscribe-form").on("submit", function (e) {
-      e.preventDefault();
-      var $el = $(this),
-        $alert = $el.find(".form-validation"),
-        $submit = $el.find("button"),
-        action = $el.attr("action");
-      $submit.button("loading");
-      $alert.removeClass("alert-danger alert-success");
-      $alert.html("");
-      $.ajax({
-        type: "POST",
-        url: action,
-        data: $el.serialize() + "&ajax=1",
-        dataType: "JSON",
-        success: function (response) {
-          if (response.status == "error") {
-            $alert.html(response.message);
-            $alert.addClass("alert-danger").fadeIn(500);
-          } else {
-            $el.trigger("reset");
-            $alert.html(response.message);
-            $alert.addClass("alert-success").fadeIn(500);
-          }
-          $submit.button("reset");
-        },
-      });
-    });
-  });
+  // $(document).on("ready", function () {
+  //   $("#subscribe-form").on("submit", function (e) {
+  //     e.preventDefault();
+  //     var $el = $(this),
+  //       $alert = $el.find(".form-validation"),
+  //       $submit = $el.find("button"),
+  //       action = $el.attr("action");
+  //     $submit.button("loading");
+  //     $alert.removeClass("alert-danger alert-success");
+  //     $alert.html("");
+  //     $.ajax({
+  //       type: "POST",
+  //       url: action,
+  //       data: $el.serialize() + "&ajax=1",
+  //       dataType: "JSON",
+  //       success: function (response) {
+  //         if (response.status == "error") {
+  //           $alert.html(response.message);
+  //           $alert.addClass("alert-danger").fadeIn(500);
+  //         } else {
+  //           $el.trigger("reset");
+  //           $alert.html(response.message);
+  //           $alert.addClass("alert-success").fadeIn(500);
+  //         }
+  //         $submit.button("reset");
+  //       },
+  //     });
+  //   });
+  // });
   /* -------------------
     Bootstrap Tooltip, Alert, Tabs
     ---------------------*/
@@ -404,157 +353,157 @@
 /* -------------------
 Portfolio
  ---------------------*/
-(function ($, window, document, undefined) {
-  "use strict";
+// (function ($, window, document, undefined) {
+//   "use strict";
 
-  var gridContainer = $("#grid-container-fullwidth"),
-    filtersContainer = $("#filters-container-fullwidth"),
-    wrap,
-    filtersCallback;
+//   var gridContainer = $("#grid-container-fullwidth"),
+//     filtersContainer = $("#filters-container-fullwidth"),
+//     wrap,
+//     filtersCallback;
 
-  gridContainer.cubeportfolio({
-    defaultFilter: "*",
-    animationType: "fadeOut",
-    gapHorizontal: 0,
-    gapVertical: 0,
-    gridAdjustment: "responsive",
-    caption: "zoom",
-    displayType: "lazyLoading",
-    displayTypeSpeed: 100,
-    // lightbox
-    lightboxDelegate: ".cbp-lightbox",
-    lightboxGallery: true,
-    lightboxTitleSrc: "data-title",
-    lightboxCounter:
-      '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
-    // singlePage popup
-    singlePageDelegate: ".cbp-singlePage",
-    singlePageDeeplinking: true,
-    singlePageStickyNavigation: true,
-    singlePageCounter:
-      '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
-    singlePageCallback: function (url, element) {
-      // to update singlePage content use the following method: this.updateSinglePage(yourContent)
-      var t = this;
-      $.ajax({
-        url: url,
-        type: "GET",
-        dataType: "html",
-        timeout: 5000,
-      })
-        .done(function (result) {
-          t.updateSinglePage(result);
-        })
-        .fail(function () {
-          t.updateSinglePage("Error! Please refresh the page!");
-        });
-    },
-    // single page inline
-    singlePageInlineDelegate: ".cbp-singlePageInline",
-    singlePageInlinePosition: "above",
-    singlePageInlineInFocus: true,
-    singlePageInlineCallback: function (url, element) {
-      // to update singlePage Inline content use the following method: this.updateSinglePageInline(yourContent)
-    },
-  });
-  /* add listener for filters */
-  if (filtersContainer.hasClass("cbp-l-filters-dropdown")) {
-    wrap = filtersContainer.find(".cbp-l-filters-dropdownWrap");
-    wrap.on({
-      "mouseover.cbp": function () {
-        wrap.addClass("cbp-l-filters-dropdownWrap-open");
-      },
-      "mouseleave.cbp": function () {
-        wrap.removeClass("cbp-l-filters-dropdownWrap-open");
-      },
-    });
-    filtersCallback = function (me) {
-      wrap.find(".cbp-filter-item").removeClass("cbp-filter-item-active");
-      wrap.find(".cbp-l-filters-dropdownHeader").text(me.text());
-      me.addClass("cbp-filter-item-active");
-      wrap.trigger("mouseleave.cbp");
-    };
-  } else {
-    filtersCallback = function (me) {
-      me.addClass("cbp-filter-item-active")
-        .siblings()
-        .removeClass("cbp-filter-item-active");
-    };
-  }
-  filtersContainer.on("click.cbp", ".cbp-filter-item", function () {
-    var me = $(this);
-    if (me.hasClass("cbp-filter-item-active")) {
-      return;
-    }
-    // get cubeportfolio data and check if is still animating (reposition) the items.
-    if (!$.data(gridContainer[0], "cubeportfolio").isAnimating) {
-      filtersCallback.call(null, me);
-    }
-    // filter the items
-    gridContainer.cubeportfolio("filter", me.data("filter"), function () {});
-  });
-  /* activate counter for filters */
-  gridContainer.cubeportfolio(
-    "showCounter",
-    filtersContainer.find(".cbp-filter-item"),
-    function () {
-      // read from url and change filter active
-      var match = /#cbpf=(.*?)([#|?&]|$)/gi.exec(location.href),
-        item;
-      if (match !== null) {
-        item = filtersContainer
-          .find(".cbp-filter-item")
-          .filter('[data-filter="' + match[1] + '"]');
-        if (item.length) {
-          filtersCallback.call(null, item);
-        }
-      }
-    }
-  );
-  /* add listener for load more */
-  $(".cbp-l-loadMore-button-link").on("click.cbp", function (e) {
-    e.preventDefault();
-    var clicks,
-      me = $(this),
-      oMsg;
-    if (me.hasClass("cbp-l-loadMore-button-stop")) {
-      return;
-    }
-    // get the number of times the loadMore link has been clicked
-    clicks = $.data(this, "numberOfClicks");
-    clicks = clicks ? ++clicks : 1;
-    $.data(this, "numberOfClicks", clicks);
-    // set loading status
-    oMsg = me.text();
-    me.text("LOADING...");
-    // perform ajax request
-    $.ajax({
-      url: me.attr("href"),
-      type: "GET",
-      dataType: "HTML",
-    })
-      .done(function (result) {
-        var items, itemsNext;
-        // find current container
-        items = $(result).filter(function () {
-          return $(this).is("div" + ".cbp-loadMore-block" + clicks);
-        });
-        gridContainer.cubeportfolio("appendItems", items.html(), function () {
-          // put the original message back
-          me.text(oMsg);
-          // check if we have more works
-          itemsNext = $(result).filter(function () {
-            return $(this).is("div" + ".cbp-loadMore-block" + (clicks + 1));
-          });
+//   gridContainer.cubeportfolio({
+//     defaultFilter: "*",
+//     animationType: "fadeOut",
+//     gapHorizontal: 0,
+//     gapVertical: 0,
+//     gridAdjustment: "responsive",
+//     caption: "zoom",
+//     displayType: "lazyLoading",
+//     displayTypeSpeed: 100,
+//     // lightbox
+//     lightboxDelegate: ".cbp-lightbox",
+//     lightboxGallery: true,
+//     lightboxTitleSrc: "data-title",
+//     lightboxCounter:
+//       '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+//     // singlePage popup
+//     singlePageDelegate: ".cbp-singlePage",
+//     singlePageDeeplinking: true,
+//     singlePageStickyNavigation: true,
+//     singlePageCounter:
+//       '<div class="cbp-popup-singlePage-counter">{{current}} of {{total}}</div>',
+//     singlePageCallback: function (url, element) {
+//       // to update singlePage content use the following method: this.updateSinglePage(yourContent)
+//       var t = this;
+//       $.ajax({
+//         url: url,
+//         type: "GET",
+//         dataType: "html",
+//         timeout: 5000,
+//       })
+//         .done(function (result) {
+//           t.updateSinglePage(result);
+//         })
+//         .fail(function () {
+//           t.updateSinglePage("Error! Please refresh the page!");
+//         });
+//     },
+//     // single page inline
+//     singlePageInlineDelegate: ".cbp-singlePageInline",
+//     singlePageInlinePosition: "above",
+//     singlePageInlineInFocus: true,
+//     singlePageInlineCallback: function (url, element) {
+//       // to update singlePage Inline content use the following method: this.updateSinglePageInline(yourContent)
+//     },
+//   });
+//   /* add listener for filters */
+//   if (filtersContainer.hasClass("cbp-l-filters-dropdown")) {
+//     wrap = filtersContainer.find(".cbp-l-filters-dropdownWrap");
+//     wrap.on({
+//       "mouseover.cbp": function () {
+//         wrap.addClass("cbp-l-filters-dropdownWrap-open");
+//       },
+//       "mouseleave.cbp": function () {
+//         wrap.removeClass("cbp-l-filters-dropdownWrap-open");
+//       },
+//     });
+//     filtersCallback = function (me) {
+//       wrap.find(".cbp-filter-item").removeClass("cbp-filter-item-active");
+//       wrap.find(".cbp-l-filters-dropdownHeader").text(me.text());
+//       me.addClass("cbp-filter-item-active");
+//       wrap.trigger("mouseleave.cbp");
+//     };
+//   } else {
+//     filtersCallback = function (me) {
+//       me.addClass("cbp-filter-item-active")
+//         .siblings()
+//         .removeClass("cbp-filter-item-active");
+//     };
+//   }
+//   filtersContainer.on("click.cbp", ".cbp-filter-item", function () {
+//     var me = $(this);
+//     if (me.hasClass("cbp-filter-item-active")) {
+//       return;
+//     }
+//     // get cubeportfolio data and check if is still animating (reposition) the items.
+//     if (!$.data(gridContainer[0], "cubeportfolio").isAnimating) {
+//       filtersCallback.call(null, me);
+//     }
+//     // filter the items
+//     gridContainer.cubeportfolio("filter", me.data("filter"), function () {});
+//   });
+//   /* activate counter for filters */
+//   gridContainer.cubeportfolio(
+//     "showCounter",
+//     filtersContainer.find(".cbp-filter-item"),
+//     function () {
+//       // read from url and change filter active
+//       var match = /#cbpf=(.*?)([#|?&]|$)/gi.exec(location.href),
+//         item;
+//       if (match !== null) {
+//         item = filtersContainer
+//           .find(".cbp-filter-item")
+//           .filter('[data-filter="' + match[1] + '"]');
+//         if (item.length) {
+//           filtersCallback.call(null, item);
+//         }
+//       }
+//     }
+//   );
+//   /* add listener for load more */
+//   $(".cbp-l-loadMore-button-link").on("click.cbp", function (e) {
+//     e.preventDefault();
+//     var clicks,
+//       me = $(this),
+//       oMsg;
+//     if (me.hasClass("cbp-l-loadMore-button-stop")) {
+//       return;
+//     }
+//     // get the number of times the loadMore link has been clicked
+//     clicks = $.data(this, "numberOfClicks");
+//     clicks = clicks ? ++clicks : 1;
+//     $.data(this, "numberOfClicks", clicks);
+//     // set loading status
+//     oMsg = me.text();
+//     me.text("LOADING...");
+//     // perform ajax request
+//     $.ajax({
+//       url: me.attr("href"),
+//       type: "GET",
+//       dataType: "HTML",
+//     })
+//       .done(function (result) {
+//         var items, itemsNext;
+//         // find current container
+//         items = $(result).filter(function () {
+//           return $(this).is("div" + ".cbp-loadMore-block" + clicks);
+//         });
+//         gridContainer.cubeportfolio("appendItems", items.html(), function () {
+//           // put the original message back
+//           me.text(oMsg);
+//           // check if we have more works
+//           itemsNext = $(result).filter(function () {
+//             return $(this).is("div" + ".cbp-loadMore-block" + (clicks + 1));
+//           });
 
-          if (itemsNext.length === 0) {
-            me.text("NO MORE WORKS");
-            me.addClass("cbp-l-loadMore-button-stop");
-          }
-        });
-      })
-      .fail(function () {
-        // error
-      });
-  });
-})(jQuery, window, document);
+//           if (itemsNext.length === 0) {
+//             me.text("NO MORE WORKS");
+//             me.addClass("cbp-l-loadMore-button-stop");
+//           }
+//         });
+//       })
+//       .fail(function () {
+//         // error
+//       });
+//   });
+// })(jQuery, window, document);
